8fdbaf54442b73eab2b17e35ebc22a4bfcd14b2d
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-08-17 09:48:26 +0200

    Header formatting

--- lq.go
+++ lq.go
@@ -1,51 +1,47 @@
-// This utility is a spatial database which stores objects each of
-// which is associated with a 3d point (a location in a 3d space).
-// The points serve as the "search key" for the associated object.
-// It is intended to efficiently answer "sphere inclusion" queries,
-// also known as range queries: basically questions like:
-
-//   Which objects are within a radius R of the location L?
-
-// In this context, "efficiently" means significantly faster than the
-// naive, brute force O(n) testing of all known points.  Additionally
-// it is assumed that the objects move along unpredictable paths, so
-// that extensive preprocessing (for example, constructing a Delaunay
-// triangulation of the point set) may not be practical.
-
-// The implementation is a "bin lattice": a 3d rectangular array of
-// brick-shaped (rectangular parallelepipeds) regions of space.  Each
-// region is represented by a pointer to a (possibly empty) doubly-
-// linked list of objects.  All of these sub-bricks are the same
-// size.  All bricks are aligned with the global coordinate axes.
-
-// Terminology used here: the region of space associated with a bin
-// is called a sub-brick.  The collection of all sub-bricks is called
-// the super-brick.  The super-brick should be specified to surround
-// the region of space in which (almost) all the key-points will
-// exist.  If key-points move outside the super-brick everything will
-// continue to work, but without the speed advantage provided by the
-// spatial subdivision.  For more details about how to specify the
-// super-brick's position, size and subdivisions see lqCreateDatabase
-// below.
-
-// Overview of usage: an application using this facility would first
-// create a database with lqCreateDatabase.  For each client object
-// the application wants to put in the database it creates a
-// lqClientProxy and initializes it with lqInitClientProxy.  When a
-// client object moves, the application calls lqUpdateForNewLocation.
-// To perform a query MapOverAllObjectsInLocality is passed an
-// application-supplied call-back function to be applied to all
-// client objects in the locality.  See lqCallBackFunction below for
-// more detail.  The lqFindNearestNeighborWithinRadius function can
-// be used to find a single nearest neighbor using the database.
-
-// Note that "locality query" is also known as neighborhood query,
-// neighborhood search, near neighbor search, and range query.  For
-// additional information on this and related topics see:
-// http://www.red3d.com/cwr/boids/ips.html
-
-// For some description and illustrations of this database in use,
-// see this paper: http://www.red3d.com/cwr/papers/2000/pip.html
+// This utility is a spatial database which stores objects each of which is
+// associated with a 3d point (a location in a 3d space).  The points serve as
+// the "search key" for the associated object.  It is intended to efficiently
+// answer "sphere inclusion" queries, also known as range queries: basically
+// questions like:
+//
+//    Which objects are within a radius R of the location L?
+//
+// In this context, "efficiently" means significantly faster than the naive,
+// brute force O(n) testing of all known points.  Additionally it is assumed that
+// the objects move along unpredictable paths, so that extensive preprocessing
+// (for example, constructing a Delaunay triangulation of the point set) may not
+// be practical.
+//
+// The implementation is a "bin lattice": a 3d rectangular array of brick-shaped
+// (rectangular parallelepipeds) regions of space.  Each region is represented by
+// a pointer to a (possibly empty) doubly- linked list of objects.  All of these
+// sub-bricks are the same size.  All bricks are aligned with the global
+// coordinate axes.
+//
+// Terminology used here: the region of space associated with a bin is called a
+// sub-brick.  The collection of all sub-bricks is called the super-brick.  The
+// super-brick should be specified to surround the region of space in which
+// (almost) all the key-points will exist.  If key-points move outside the
+// super-brick everything will continue to work, but without the speed advantage
+// provided by the spatial subdivision.  For more details about how to specify
+// the super-brick's position, size and subdivisions see CreateDatabase below.
+//
+// Overview of usage: an application using this facility would first create a
+// database with CreateDatabase.  For each client object the application wants to
+// put in the database it creates a ClientProxy and initializes it with
+// InitClientProxy.  When a client object moves, the application calls
+// UpdateForNewLocation.  To perform a query MapOverAllObjectsInLocality is
+// passed an application-supplied call-back function to be applied to all client
+// objects in the locality.  See CallBackFunction below for more detail.  The
+// FindNearestNeighborWithinRadius function can be used to find a single nearest
+// neighbor using the database.
+//
+// Note that "locality query" is also known as neighborhood query, neighborhood
+// search, near neighbor search, and range query.  For additional information on
+// this and related topics see: http://www.red3d.com/cwr/boids/ips.html
+//
+// For some description and illustrations of this database in use, see this
+// paper: http://www.red3d.com/cwr/papers/2000/pip.html
 package lq
 
 import "math"
