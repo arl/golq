bc9e68050a74d695a66868373faa179f80a0f762
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-08-19 11:32:04 +0200

    Add 2 insertion tests

--- lq_test.go
+++ lq_test.go
@@ -1,19 +1,54 @@
 package lq
 
-import (
-	"fmt"
-	"log"
-	"testing"
+import "testing"
+
+// create our mock entity for test, it's just an int
+func newEntity(id int) *ClientProxy {
+	return NewClientProxy(id)
+}
+
+// convenience types
+type (
+	idMap  map[int]bool // map of ints, acting as a set of int
+	idList []int        // slice of ints
 )
 
-func TestDatabase(t *testing.T) {
-	fmt.Println("Hello world!")
-	log.Println("Hello world!")
+func (m idMap) assertContains(t *testing.T, id int) {
+	if _, ok := m[1]; !ok {
+		t.Errorf("ids map was expected to contain id:", id)
+	}
+}
 
+// CallBackFunction that copies every found entity id into the provided idMap or idList
+func retrieveAllIds(clientObject interface{}, distanceSquared float64, clientQueryState interface{}) {
+	switch clientQueryState.(type) {
+	case idList:
+		s := clientQueryState.(idList)
+		s = append(s, clientObject.(int))
+	case idMap:
+		m := clientQueryState.(idMap)
+		m[clientObject.(int)] = true
+	}
+}
+
+func TestAddObjectToSuperBrick(t *testing.T) {
 	db := CreateDatabase(0, 0, 10, 10, 5, 5)
-	db.RemoveAllObjects()
 
-	f := 3.14
-	bin := NewClientProxy(f)
-	fmt.Println(bin)
+	p1 := newEntity(1)
+	db.UpdateForNewLocation(p1, 2, 0)
+
+	ids := idMap{}
+	db.MapOverAllObjects(retrieveAllIds, ids)
+	ids.assertContains(t, 1)
+}
+
+func TestAddObjectOutOfSuperBrick(t *testing.T) {
+	db := CreateDatabase(0, 0, 10, 10, 5, 5)
+
+	p1 := newEntity(1)
+	db.UpdateForNewLocation(p1, 11, 0)
+
+	ids := idMap{}
+	db.MapOverAllObjects(retrieveAllIds, ids)
+	ids.assertContains(t, 1)
 }
